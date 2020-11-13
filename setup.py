1457d81cd7adec56c555bb3f13437506f41c2f10
Adit Aggarwal <adit6859@gmail.com>
2020-11-13 20:25:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
   author = 'Adit Aggarwal',                   # Type in your name
   author_email = 'adit6859@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/adit6859',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/adit6859/TOPSIS-Adit-101803147/archive/v0.6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/adit6859/TOPSIS_Adit_101803147/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['TOPSIS', '101803147', 'Adit Aggarwal'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
